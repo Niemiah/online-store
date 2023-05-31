a3ef89411ea1ae905aa2a8589fbade5bbbce22fd
Niemiah <117840352+Niemiah@users.noreply.github.com>
2023-05-31 13:42:16 -0600

    Update 1

--- src/main/java/com/solvd/online/store/addresses/Addresses.java
+++ src/main/java/com/solvd/online/store/addresses/Addresses.java
@@ -1,4 +1,43 @@
 package com.solvd.online.store.addresses;
 
 public class Addresses {
+    private int addressId;
+    private String address;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String country;
+
+    public Addresses(int addressId, String address, String city, String state, String postalCode, String country) {
+        this.addressId = addressId;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.postalCode = postalCode;
+        this.country = country;
+    }
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
 }
