3d12408b868b6eca879bfdcdba82e69eeae8edc1
Niemiah <117840352+Niemiah@users.noreply.github.com>
2023-06-09 07:44:51 -0600

    Update 1

--- src/main/java/com/solvd/online/store/service/ICategoryService.java
+++ src/main/java/com/solvd/online/store/service/ICategoryService.java
@@ -1,4 +1,11 @@
 package com.solvd.online.store.service;
 
+import com.solvd.online.store.model.Category;
+
 public interface ICategoryService {
+    void saveCategoryToDB(Category category);
+
+    void updateCategoryInDB(Category category);
+
+    Category getCategoryInDB(int id);
 }
--- src/main/java/com/solvd/online/store/service/impl/CategoryService.java
+++ src/main/java/com/solvd/online/store/service/impl/CategoryService.java
@@ -1,5 +1,27 @@
 package com.solvd.online.store.service.impl;
+import com.solvd.online.store.dao.ICategoryDAO;
+import com.solvd.online.store.model.Category;
+import com.solvd.online.store.impl.CategoryDAO;
 import com.solvd.online.store.service.ICategoryService;
 
+
 public class CategoryService implements ICategoryService {
+
+    @Override
+    public void saveCategoryToDB(Category category) {
+        ICategoryDAO categoryDao = new CategoryDAO();
+        categoryDao.insert(category);
+    }
+
+    @Override
+    public void updateCategoryInDB(Category category) {
+        ICategoryDAO categoryDao = new CategoryDAO();
+        categoryDao.update(category);
+    }
+
+    @Override
+    public Category getCategoryInDB(int id) {
+        ICategoryDAO categoryDao = new CategoryDAO();
+        return categoryDao.getById(id);
+    }
 }
