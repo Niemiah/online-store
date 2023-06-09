a028ee0c5a50506358b35bacabc880b52afc2941
Niemiah <117840352+Niemiah@users.noreply.github.com>
2023-06-09 07:47:11 -0600

    Update 1

--- src/main/java/com/solvd/online/store/service/IOrderDetailService.java
+++ src/main/java/com/solvd/online/store/service/IOrderDetailService.java
@@ -1,4 +1,8 @@
 package com.solvd.online.store.service;
+import com.solvd.online.store.model.OrderDetail;
 
 public interface IOrderDetailService {
+    void saveOrderDetailToDB(OrderDetail orderDetail);
+    void updateOrderDetailInDB(OrderDetail orderDetail);
+    OrderDetail getOrderDetailInDB(int id);
 }
--- src/main/java/com/solvd/online/store/service/impl/OrderDetailService.java
+++ src/main/java/com/solvd/online/store/service/impl/OrderDetailService.java
@@ -1,5 +1,27 @@
 package com.solvd.online.store.service.impl;
+import com.solvd.online.store.dao.IOrderDetailDAO;
+import com.solvd.online.store.impl.OrderDetailDAO;
+import com.solvd.online.store.model.OrderDetail;
 import com.solvd.online.store.service.IOrderDetailService;
 
+
 public class OrderDetailService implements IOrderDetailService {
+
+    @Override
+    public void saveOrderDetailToDB(OrderDetail orderDetail) {
+        IOrderDetailDAO orderDetailDao = new OrderDetailDAO();
+        orderDetailDao.insert(orderDetail);
+    }
+
+    @Override
+    public void updateOrderDetailInDB(OrderDetail orderDetail) {
+        IOrderDetailDAO orderDetailDao = new OrderDetailDAO();
+        orderDetailDao.update(orderDetail);
+    }
+
+    @Override
+    public OrderDetail getOrderDetailInDB(int id) {
+        IOrderDetailDAO orderDetailDao = new OrderDetailDAO();
+        return orderDetailDao.getById(id);
+    }
 }
