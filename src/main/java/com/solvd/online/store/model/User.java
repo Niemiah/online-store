537d67b853f4004a6b3e6c6817e9138c8070dcf1
Niemiah <117840352+Niemiah@users.noreply.github.com>
2023-06-15 23:59:50 -0600

    Update 1

--- src/main/java/com/solvd/online/store/model/User.java
+++ src/main/java/com/solvd/online/store/model/User.java
@@ -1,51 +1,31 @@
 package com.solvd.online.store.model;
-
+import java.util.ArrayList;
+import java.util.List;
 public class User {
     private int userId;
     private String firstName;
     private String lastName;
     private String email;
+    private List<Order> orders = new ArrayList<>();
+    private List<Payment> payments = new ArrayList<>();
+    private List<Address> addresses = new ArrayList<>();
+    private List<Review> reviews = new ArrayList<>();
 
-    public User(int userId, String firstName, String lastName, String email) {
-        this.userId = userId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
-
-    public User() {
-
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    // Getters and setters
+    public int getUserId() { return userId; }
+    public void setUserId(int userId) { this.userId = userId; }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public List<Order> getOrders() { return orders; }
+    public void setOrders(List<Order> orders) { this.orders = orders; }
+    public List<Payment> getPayments() { return payments; }
+    public void setPayments(List<Payment> payments) { this.payments = payments; }
+    public List<Address> getAddresses() { return addresses; }
+    public void setAddresses(List<Address> addresses) { this.addresses = addresses; }
+    public List<Review> getReviews() { return reviews; }
+    public void setReviews(List<Review> reviews) { this.reviews = reviews; }
 }
